ad455c38166d9218e08b8f8d46182745d7870151
thomas_mary <tmary@oui.sncf>
2022-03-04 10:44:38 +0100

    kdo

--- projet_ticket/Ligne.cs
+++ projet_ticket/Ligne.cs
@@ -8,11 +8,22 @@ namespace projet_ticket
 {
     public class Ligne
     {
-        public Article Article { get; set; }
-        public int Quantite { get; set; }
-        public int TotalHT { get; set; }
-        public int TotalTTC { get; set; }
-        public int TauxTVA { get; set; }
-        public string CodeTVA { get; set; }
+        public Ligne(Article article, int quantite, int totalHT, int totalTTC, int tauxTVA, string codeTVA)
+        {
+            this.article = article;
+            this.quantite = quantite;
+            this.totalHT = totalHT;
+            this.totalTTC = totalTTC;
+            this.tauxTVA = tauxTVA;
+            this.codeTVA = codeTVA;
+        }
+
+        public Article article { get; set; }
+        public int quantite { get; set; }
+        public int totalHT { get; set; }
+        public int totalTTC { get; set; }
+        public int tauxTVA { get; set; }
+        public string codeTVA { get; set; }
     }
+
 }
